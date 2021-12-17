2a0e8ffc8059b12297f342625707a456b3256ab9
Suprem Vanam <supremvanam.gsp@gmail.com>
2021-12-17 02:15:36 -0500

    QR Code Refresh

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,42 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import CartSingleItem from "./CartSingleItem";
-import { getAllOrder } from '../Controller/api';
-import { NavLink } from 'react-router-dom';
+import { getAllOrder } from "../Controller/api";
+import { NavLink } from "react-router-dom";
 
 function CartItem() {
-  
   const [order, setOrder] = useState([]);
-    useEffect(() => {
-      getOrders();
-    }, [])
+  useEffect(() => {
+    getOrders();
+  }, []);
 
-    const getOrders = async () =>{
-        const response = await getAllOrder();
-        //console.log(response.data);
-        setOrder(response.data);
-    }
-  
+  const getOrders = async () => {
+    const response = await getAllOrder();
+    //console.log(response.data);
+    setOrder(response.data);
+  };
+
   return (
     <div className="card">
-        <h2 style={{marginBottom:"40px",textAlign:"center"}}>CART</h2>
+      <h2 style={{ marginBottom: "40px", textAlign: "center" }}>CART</h2>
 
-        {
-            order.map((data) => (
-              <CartSingleItem key={data.id} itemId={data.id} itemTitle={data.name} itemPrice={data.price} />
-            ))
-        }
-        
-        <div className="product-item" style={{marginTop:"50px"}}>
+      {order.map((data) => (
+        <CartSingleItem
+          key={data.id}
+          itemId={data.id}
+          itemTitle={data.name}
+          itemPrice={data.price}
+        />
+      ))}
+
+      <div className="product-item" style={{ marginTop: "50px" }}>
         <span className="item-id"></span>
-            <h4 className="item-name" style={{paddingLeft:"40px"}}>Total</h4>
-            <h4 className="item-price">$ { order.reduce((a,b) =>  a = a + b.price , 0 ) }</h4>
-       </div>
-        <div className="item-center" style={{marginTop:"40px"}}>
-            <NavLink to="/pay" className="btn text-center"> Pay Now </NavLink>
-        </div>
-
+        <h4 className="item-name" style={{ paddingLeft: "40px" }}>
+          Total
+        </h4>
+        <h4 className="item-price">
+          $ {order.reduce((a, b) => (a = a + b.price), 0)}
+        </h4>
+      </div>
+      <div className="item-center" style={{ marginTop: "40px" }}>
+        <NavLink to="/pay" className="btn text-center">
+          {" "}
+          Pay Now{" "}
+        </NavLink>
+      </div>
     </div>
-    
   );
 }
 
--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
+import Html5QrcodePlugin from "./Html5QrcodePlugin.jsx";
+import logoPNG from "../images/qrder-logo.png";
+import { NavLink } from "react-router-dom";
 
-const QRCode = () => {
-  return <div></div>;
-};
+export class QRCode extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      decodedResults: [],
+    };
+
+    // This binding is necessary to make `this` work in the callback.
+    this.onNewScanResult = this.onNewScanResult.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <img className="logo" src={logoPNG} />
+        &nbsp;&nbsp;
+        <section className="App-section">
+          <Html5QrcodePlugin
+            fps={10}
+            qrbox={250}
+            disableFlip={true}
+            qrCodeSuccessCallback={this.onNewScanResult}
+          />
+        </section>
+        &nbsp;
+        <div className="item-center">
+          <NavLink to="/product" className="btn text-center">
+            {" "}
+            View Menu{" "}
+          </NavLink>
+        </div>
+      </div>
+    );
+  }
+
+  // Redirect to the menu page - WIP
+  onNewScanResult(decodedText, decodedResult) {
+    console.log("App [result]", decodedResult.decodedText);
+
+    // let decodedResults = this.state.decodedResults;
+    // decodedResults.push(decodedResult);
+    this.setState((state, props) => {
+      state.decodedResults.push(decodedResult);
+      return state;
+    });
+  }
+}
 
 export default QRCode;
