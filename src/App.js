3c88ef989bfec12777114382ead0570b86b7d16b
Suprem Vanam <supremvanam.gsp@gmail.com>
2021-12-19 10:36:27 -0500

    Reroute pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./components/Home";
 import QRCode from "./components/QRCode";
+import Tables from "./components/Tables";
 import Product from "./components/Product";
 import CartItem from "./components/CartItem";
 import OrderItem from "./components/OrderItem";
 import TableSelection from "./components/TableSelection";
 import WaitStaff from "./components/WaitStaff";
 import QRScanner from "./components/QRScanner";
+import OrderSuccess from "./components/OrderSuccess";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -16,9 +18,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/scan" element={<QRScanner />} />
 
-        <Route path="/product" element={<Product />} />
+        <Route path="/tables/:id" element={<Tables />} />
+        <Route path="/product/:id/:tableId" element={<Product />} />
         <Route path="/cart" element={<CartItem />} />
-        <Route path="/pay" element={<OrderItem />} />
+        <Route path="/success" element={<OrderSuccess />} />
+
+        <Route path="/pay/:id" element={<OrderItem />} />
 
         <Route path="/staff" element={<WaitStaff />} />
         <Route path="/selecttable" element={<TableSelection />} />
